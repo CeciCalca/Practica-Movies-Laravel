5906636f21c7c5cdcc10fda3a583ccf443a1d39f
Your Name <you@example.com>
2019-06-28 14:48:31 -0300

    cambios en nav bar

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -5,21 +5,17 @@
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
+    <ul class="navbar-nav mr-auto ">
+      <li class="nav-item">
         <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="/movies">Movies</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item ">
         <a class="nav-link" href="/actors">Actors</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
 <br>
